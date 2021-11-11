b0cdebd7d6b869e2d8d7426f37f5071d38de925f
wijipermana <wiji.permana@gmail.com>
2021-11-11 13:54:34 +0700

    lesson 8 Using State

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 function Home() {
-  const handleClick = (e) => {
-    console.log("hello, ninjas", e.target);
+  const [name, setName] = useState("mario");
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setName("luigi");
   };
 
-  const handleClickToo = (name, e) => {
-    console.log("hello " + name, e.target);
+  const increaseCount = () => {
+    setCount(count + 1);
+  }
+  const decreaseCount = () => {
+    setCount(count - 1);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>click me</button>
-      <button onClick={(e) => handleClickToo("ninja", e)}>click me too</button>
+      <button onClick={increaseCount}>{count}</button>
+      <button onClick={decreaseCount}>{count}</button>
     </div>
   );
 }
