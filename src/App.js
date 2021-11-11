0ea5c63d16b413e855792b064b16f854ff64506f
wijipermana <wiji.permana@gmail.com>
2021-11-11 13:30:18 +0700

    lesson 5 Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "The Title";
-  const likes = 50;
-  const array = [1, 2, 3, 4, 5];
-  const random = Math.random() * 10;
-  const link = "https://www.google.com";
-  // const person = { name: "yoshi", age: 30 };
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{array}</p>
-        <p>{random}</p>
-        <a href={link} target="_blank">Google Site</a>
+        <Home />
       </div>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div class="home">
+      <h2>Homepage</h2>
+    </div>
+  );
+}
+
+export default Home;
