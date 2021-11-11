2bcc0f341ad742c97ccbcab2ee6274898d0c83ff
wijipermana <wiji.permana@gmail.com>
2021-11-11 13:37:55 +0700

    lesson 6 Adding Styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ function Navbar() {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: "#f1356d",
+          borderRadius: "8px",
+        }}>New Blog</a>
       </div>
     </nav>
   );
