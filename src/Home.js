60d4bcb1dd4b649e6d00495d775554519b9b6dd2
wijipermana <wiji.permana@gmail.com>
2021-11-11 13:46:50 +0700

    lesson 7 Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 function Home() {
+  const handleClick = (e) => {
+    console.log("hello, ninjas", e.target);
+  };
+
+  const handleClickToo = (name, e) => {
+    console.log("hello " + name, e.target);
+  }
+
   return (
-    <div class="home">
+    <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>click me</button>
+      <button onClick={(e) => handleClickToo("ninja", e)}>click me too</button>
     </div>
   );
 }
