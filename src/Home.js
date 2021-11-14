52f928668cade898103ab5c52f3695dabc656c60
wijipermana <wiji.permana@gmail.com>
2021-11-14 12:42:08 +0700

    lesson 15 useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -13,6 +13,8 @@ function Home() {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -20,12 +22,14 @@ function Home() {
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
