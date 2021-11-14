51347bc42be6b93ba4e081237cfda086b4372d0f
wijipermana <wiji.permana@gmail.com>
2021-11-14 12:35:58 +0700

    lesson 14 useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 function Home() {
@@ -17,6 +17,11 @@ function Home() {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(blogs);
+  });
 
   return (
     <div className="home">
