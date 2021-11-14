210d459924fdfe2b20d2b0e49b77f806850338af
wijipermana <wiji.permana@gmail.com>
2021-11-14 10:57:08 +0700

    lesson 13 Function as Props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-function BlogList({ blogs, title }) {
+function BlogList({ blogs, title, handleDelete }) {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -6,6 +6,7 @@ function BlogList({ blogs, title }) {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -13,13 +13,14 @@ function Home() {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "mario")}
-        title="Mario's Blogs"
-      />
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
     </div>
   );
 }
