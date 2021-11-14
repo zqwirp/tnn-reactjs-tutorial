377fcdc82a9c1d4c3d425e12ceeaa9d5565a63c3
wijipermana <wiji.permana@gmail.com>
2021-11-14 13:04:03 +0700

    lesson 17 Fetching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,10 @@
-function BlogList({ blogs, title, handleDelete }) {
+function BlogList({ blogs }) {
   return (
     <div className="blog-list">
-      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
