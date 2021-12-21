e0a07fd070bdcd0803a3f56c4e1e2489eb06a58b
zqwirp <wiji.permana@gmail.com>
2021-12-21 15:59:21 +0700

    lesson 19 Handling Fetch Error

---
+++ json-server.sh
@@ -0,0 +1,1 @@
+npx json-server --watch db/data.json --port 8000
--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,32 @@ import BlogList from "./BlogList";
 function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-        .then((response) => response.json())
-        .then((data) => {
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("could not fetch the data");
+          }
+          return response.json();
+        })
+        .then(data => {
+          setError(null);
           setIsPending(false);
           setBlogs(data);
+        })
+        .catch(error => {
+          setIsPending(false);
+          setError(error.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
